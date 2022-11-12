77726888d5dfc038f812dfea85f8cac57d860735
Richona <105828822+Richona@users.noreply.github.com>
2022-11-11 23:55:17 -0300

    arregle un bug en la busqueda de un titulo que no existe

--- src/controllers/moviesController.js
+++ src/controllers/moviesController.js
@@ -19,46 +19,46 @@ const moviesController = {
             include: ['genre']
         })
             .then(movies => {
-                res.render('moviesList.ejs', {movies})
+                res.render('moviesList.ejs', { movies })
             })
     },
     'detail': (req, res) => {
         db.Movie.findByPk(req.params.id,
             {
-                include : ['genre']
+                include: ['genre']
             })
             .then(movie => {
-                res.render('moviesDetail.ejs', {movie});
+                res.render('moviesDetail.ejs', { movie });
             });
     },
     'new': (req, res) => {
         db.Movie.findAll({
-            order : [
+            order: [
                 ['release_date', 'DESC']
             ],
             limit: 5
         })
             .then(movies => {
-                res.render('newestMovies', {movies});
+                res.render('newestMovies', { movies });
             });
     },
     'recomended': (req, res) => {
         db.Movie.findAll({
             include: ['genre'],
             where: {
-                rating: {[db.Sequelize.Op.gte] : 8}
+                rating: { [db.Sequelize.Op.gte]: 8 }
             },
             order: [
                 ['rating', 'DESC']
             ]
         })
             .then(movies => {
-                res.render('recommendedMovies.ejs', {movies});
+                res.render('recommendedMovies.ejs', { movies });
             });
     },
     //Aqui debo modificar para crear la funcionalidad requerida
     'buscar': async (req, res) => {
-        const {titulo} = req.query;
+        const { titulo } = req.query;
         const urlBase = "https://www.omdbapi.com/"
         const apiKey = process.env.OMDB_API_KEY
 
@@ -67,42 +67,46 @@ const moviesController = {
             let movie = await db.Movie.findOne({
                 where: {
                     title: {
-                        [Op.substring] : titulo
+                        [Op.substring]: titulo
                     }
                 }
             })
 
-            if(!movie){
+            if (!movie) {
                 let response = await fetch(`${urlBase}?apiKey=${apiKey}&t=${titulo}`);
                 movie = await response.json();
 
-                
-                const {Title, Released, imdbRating, Awards, Runtime} = movie
+                if (movie && movie.Response) {
+                    throw res.send("No hay pelicula")
+                } else {
+                    const { Title, Released, imdbRating, Awards, Runtime } = movie
 
-                /* let awardsArray = Awards.split(" ")
-                let awardsFilter = awardsArray.filter(char => !isNaN(char))
-                let awardsTotal = awardsFilter.reduce((acum, num) => +acum + +num) */
-                
-                await db.Movie.create({
-                    title: Title,
-                    rating: +imdbRating,
-                    awards: Awards.split(" ").filter(char => !isNaN(char)).reduce((acum, num) => +acum + +num),
-                    release_date: moment(Released),
-                    length: parseInt(Runtime),
-                    genre_id:null
-                })
+                    /* let awardsArray = Awards.split(" ")
+                    let awardsFilter = awardsArray.filter(char => !isNaN(char))
+                    let awardsTotal = awardsFilter.reduce((acum, num) => +acum + +num) */
 
-            }else{
-                const {title, release_date, awards, length} = movie
+
+                    await db.Movie.create({
+                        title: Title,
+                        rating: +imdbRating,
+                        awards: Awards.split(" ").filter(char => !isNaN(char)).reduce((acum, num) => +acum + +num),
+                        release_date: moment(Released),
+                        length: parseInt(Runtime),
+                        genre_id: null
+                    })
+                }
+
+            } else {
+                const { title, release_date, awards, length } = movie
                 movie = {
-                    Title : title,
+                    Title: title,
                     Poster: "http://localhost:3001/img/logo-DH.png",
                     Year: moment(release_date).format("YYYY"),
                     Awards: awards,
                     Runtime: length
                 }
             }
-            return res.render ("moviesDetailOmdb", {
+            return res.render("moviesDetailOmdb", {
                 movie
             })
         } catch (error) {
@@ -113,75 +117,81 @@ const moviesController = {
     add: function (req, res) {
         let promGenres = Genres.findAll();
         let promActors = Actors.findAll();
-        
+
         Promise
-        .all([promGenres, promActors])
-        .then(([allGenres, allActors]) => {
-            return res.render(path.resolve(__dirname, '..', 'views',  'moviesAdd'), {allGenres,allActors})})
-        .catch(error => res.send(error))
+            .all([promGenres, promActors])
+            .then(([allGenres, allActors]) => {
+                return res.render(path.resolve(__dirname, '..', 'views', 'moviesAdd'), { allGenres, allActors })
+            })
+            .catch(error => res.send(error))
     },
-    create: function (req,res) {
+    create: function (req, res) {
         Movies
-        .create(
-            {
-                title: req.body.title,
-                rating: req.body.rating,
-                awards: req.body.awards,
-                release_date: req.body.release_date,
-                length: req.body.length,
-                genre_id: req.body.genre_id
-            }
-        )
-        .then(()=> {
-            return res.redirect('/movies')})            
-        .catch(error => res.send(error))
+            .create(
+                {
+                    title: req.body.title,
+                    rating: req.body.rating,
+                    awards: req.body.awards,
+                    release_date: req.body.release_date,
+                    length: req.body.length,
+                    genre_id: req.body.genre_id
+                }
+            )
+            .then(() => {
+                return res.redirect('/movies')
+            })
+            .catch(error => res.send(error))
     },
-    edit: function(req,res) {
+    edit: function (req, res) {
         let movieId = req.params.id;
-        let promMovies = Movies.findByPk(movieId,{include: ['genre','actors']});
+        let promMovies = Movies.findByPk(movieId, { include: ['genre', 'actors'] });
         let promGenres = Genres.findAll();
         let promActors = Actors.findAll();
         Promise
-        .all([promMovies, promGenres, promActors])
-        .then(([Movie, allGenres, allActors]) => {
-            Movie.release_date = moment(Movie.release_date).format('L');
-            return res.render(path.resolve(__dirname, '..', 'views',  'moviesEdit'), {Movie,allGenres,allActors})})
-        .catch(error => res.send(error))
-    },
-    update: function (req,res) {
-        let movieId = req.params.id;
-        Movies
-        .update(
-            {
-                title: req.body.title,
-                rating: req.body.rating,
-                awards: req.body.awards,
-                release_date: req.body.release_date,
-                length: req.body.length,
-                genre_id: req.body.genre_id
-            },
-            {
-                where: {id: movieId}
+            .all([promMovies, promGenres, promActors])
+            .then(([Movie, allGenres, allActors]) => {
+                Movie.release_date = moment(Movie.release_date).format('L');
+                return res.render(path.resolve(__dirname, '..', 'views', 'moviesEdit'), { Movie, allGenres, allActors })
             })
-        .then(()=> {
-            return res.redirect('/movies')})            
-        .catch(error => res.send(error))
+            .catch(error => res.send(error))
     },
-    delete: function (req,res) {
+    update: function (req, res) {
         let movieId = req.params.id;
         Movies
-        .findByPk(movieId)
-        .then(Movie => {
-            return res.render(path.resolve(__dirname, '..', 'views',  'moviesDelete'), {Movie})})
-        .catch(error => res.send(error))
+            .update(
+                {
+                    title: req.body.title,
+                    rating: req.body.rating,
+                    awards: req.body.awards,
+                    release_date: req.body.release_date,
+                    length: req.body.length,
+                    genre_id: req.body.genre_id
+                },
+                {
+                    where: { id: movieId }
+                })
+            .then(() => {
+                return res.redirect('/movies')
+            })
+            .catch(error => res.send(error))
     },
-    destroy: function (req,res) {
+    delete: function (req, res) {
         let movieId = req.params.id;
         Movies
-        .destroy({where: {id: movieId}, force: true}) // force: true es para asegurar que se ejecute la acción
-        .then(()=>{
-            return res.redirect('/movies')})
-        .catch(error => res.send(error)) 
+            .findByPk(movieId)
+            .then(Movie => {
+                return res.render(path.resolve(__dirname, '..', 'views', 'moviesDelete'), { Movie })
+            })
+            .catch(error => res.send(error))
+    },
+    destroy: function (req, res) {
+        let movieId = req.params.id;
+        Movies
+            .destroy({ where: { id: movieId }, force: true }) // force: true es para asegurar que se ejecute la acción
+            .then(() => {
+                return res.redirect('/movies')
+            })
+            .catch(error => res.send(error))
     }
 }
 
